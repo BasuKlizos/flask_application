646bc773bbacbf47f3226e4a8c2288d83d290657
BasuKlizos <basudev@klizos.com>
2025-01-27 14:06:47 +0530

    created env file and updated in config and yml file

--- app/__init__.py
+++ app/__init__.py
@@ -3,21 +3,19 @@ from flask_jwt_extended import JWTManager
 from flask_pymongo import PyMongo
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-# from config import Config
-import os
-from urllib.parse import quote_plus
+from config import Config
 
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-jwt-secret-key")
-    MONGO_USERNAME = os.getenv("MONGO_USERNAME", "basudev")
-    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "UcjOArSo3yjUaP6C")
-    MONGO_CLUSTER = os.getenv("MONGO_CLUSTER", "questerdb.a26wb.mongodb.net")
-    MONGO_DB = os.getenv("MONGO_DB", "flask_app")
-    MONGO_URI = (
-        f"mongodb+srv://{quote_plus(MONGO_USERNAME)}:{quote_plus(MONGO_PASSWORD)}@{MONGO_CLUSTER}/"
-        f"{MONGO_DB}?retryWrites=true&w=majority"
-    )
+# class Config:
+#     SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
+#     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-jwt-secret-key")
+#     MONGO_USERNAME = os.getenv("MONGO_USERNAME", "basudev")
+#     MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "UcjOArSo3yjUaP6C")
+#     MONGO_CLUSTER = os.getenv("MONGO_CLUSTER", "questerdb.a26wb.mongodb.net")
+#     MONGO_DB = os.getenv("MONGO_DB", "flask_app")
+#     MONGO_URI = (
+#         f"mongodb+srv://{quote_plus(MONGO_USERNAME)}:{quote_plus(MONGO_PASSWORD)}@{MONGO_CLUSTER}/"
+#         f"{MONGO_DB}?retryWrites=true&w=majority"
+#     )
 
 mongo = PyMongo()
 jwt = JWTManager()
