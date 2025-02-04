eabeebe5d53fc152b3d0b1b15d977e6e63d00183
BasuKlizos <basudev@klizos.com>
2025-02-04 19:31:57 +0530

    Modified in requirements file

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -4,6 +4,7 @@ from app import mongo
 
 logging.basicConfig(level=logging.INFO)
 
+
 # For signup endpoint
 def test_signup_valid(client, user_data):
     response = client.post("/signup", json=user_data)
@@ -17,48 +18,45 @@ def test_signup_valid(client, user_data):
     mongo.db.users.delete_one({"email": user_data["email"]})
     logging.info(f"Test user deleted: {user_data["email"]}")
 
+
 def test_signup_missing_fields(client):
-    user_date = {
-        "username":"newuser",
-        "password":"1234"
-    }
-    response = client.post("/signup", json = user_date)
-    
+    user_date = {"username": "newuser", "password": "1234"}
+    response = client.post("/signup", json=user_date)
+
     assert response.status_code == 400
     assert response.json["error"] == "Invalid input"
 
-def test_signup_username_characters(client):
-    user_data = {
-        "username": "ba",
-        "email": "newuser@gmail.com",
-        "password": "1234"
-    }
 
-    response = client.post("/signup", json = user_data)
+def test_signup_username_characters(client):
+    user_data = {"username": "ba", "email": "newuser@gmail.com", "password": "1234"}
+
+    response = client.post("/signup", json=user_data)
 
     assert response.status_code == 400
     assert response.json["error"] == "Username must be between 3 and 20 characters"
+
 
 def test_signup_username_invalid(client):
     user_data = {
         "username": "newuser-1234",
         "email": "newuser1234@gmail.com",
-        "password": "12345"
+        "password": "12345",
     }
 
-    response =  client.post("/signup", json = user_data)
+    response = client.post("/signup", json=user_data)
 
     assert response.status_code == 400
     assert response.json["error"] == "Username must be alphanumeric"
 
+
 def test_signup_email_invalid(client):
     user_data = {
-        "username" : "newuser",
-        "email" : "newuser@gmail..com",
-        "password": "1234"
+        "username": "newuser",
+        "email": "newuser@gmail..com",
+        "password": "1234",
     }
 
-    response = client.post("/signup", json = user_data)
+    response = client.post("/signup", json=user_data)
 
     assert response.status_code == 400
     assert response.json["error"] == "Invalid email format"
@@ -70,31 +68,32 @@ def test_signup_username_already_exists(client, user_data, setup_user_in_db):
     assert response.status_code == 400
     assert response.json["error"] == "Username already exists"
 
+
 def test_signup_useremail_already_exists(client, user_data, setup_user_in_db):
     new_user_data = {
-        "username" : "newuser",
-        "email" : user_data["email"],
-        "password" : "1234"
+        "username": "newuser",
+        "email": user_data["email"],
+        "password": "1234",
     }
 
-    response = client.post("/signup", json = new_user_data)
+    response = client.post("/signup", json=new_user_data)
 
     assert response.status_code == 400
-    assert response.json["error"]  == "Email already exists"
+    assert response.json["error"] == "Email already exists"
+
 
 def test_signup_role_invalid(client):
     user_data = {
-        "username":"newuser",
-        "email" : "newuser@gmail.com",
+        "username": "newuser",
+        "email": "newuser@gmail.com",
         "password": "1234",
-        "role": "guest"
+        "role": "guest",
     }
 
-    response = client.post("/signup", json = user_data)
+    response = client.post("/signup", json=user_data)
 
     assert response.status_code == 400
     assert response.json["error"] == "Invalid role specified"
-
 
 
 # def test_username
@@ -102,7 +101,7 @@ def test_signup_role_invalid(client):
 
 # def test_login_valid(client, setup_user_in_db, user_data):
 #     response = client.post("/login", json={
-#         "email": user_data["email"], 
+#         "email": user_data["email"],
 #         "password": user_data["password"]
 #         })
 #     assert response.status_code == 200
@@ -117,7 +116,7 @@ def test_signup_role_invalid(client):
 #         "email": "invalid@example.com",  # Non-existent email
 #         "password": user_data["password"]
 #     })
-    
+
 #     # Assertions
 #     assert response.status_code == 401  # Unauthorized
 #     assert "error" in response.json
@@ -127,18 +126,18 @@ def test_signup_role_invalid(client):
 #     response = client.post("/login", json={"email": user_data["email"], "password": "wrongpassword"})
 #     assert response.status_code == 401
 #     assert response.json["error"] == "Invalid credentials"
-    
-    
+
+
 # def test_login_missing_fields(client):
 #     response = client.post("/login", json={})
 #     assert response.status_code == 400
 #     assert response.json["error"] == "Email and password are required"
-    
+
 # def test_login_empty_fields(client):
 #     response = client.post("/login", json={"email": "", "password": ""})
 #     assert response.status_code == 400
 #     assert response.json["error"] == "Email and password are required"
-    
+
 # def test_login_invalid_credentials(client, user_data):
 #     response = client.post("/login", json={
 #         "email": user_data["email"],
